b96243c46d72589b405d6230be384a8f34bba48a
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-02-23 13:58:51 +0600

    created backend server and expressjs api

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,21 @@
 const express = require('express');
-
+const dotenv = require('dotenv');
+const { chats } = require('./data/data');
 const app = express();
+dotenv.config();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(5000, console.log('Server started on PORT 5000'));
+app.get('/api/chat', (req, res) => {
+  res.send(chats);
+});
+
+app.get('/api/chat/:id', (req, res) => {
+  const singleChat = chats.find((chat) => chat._id === req.params.id);
+  res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
