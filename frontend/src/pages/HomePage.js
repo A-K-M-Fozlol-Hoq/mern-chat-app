b00110397c1775107856c4ffa40207af18770de1
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-03-03 00:54:27 +0600

    styled with chakra UI

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -3,8 +3,19 @@ import { Container, Box, Text } from '@chakra-ui/react';
 const HomePage = () => {
   return (
     <Container maxW="xl" centerContent>
-      <Box>
-        <Text>Talk-A-Tive</Text>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg={'white'}
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" color="black" fontFamily="work sana">
+          Talk-A-Tive
+        </Text>
       </Box>
       <Box></Box>
     </Container>
