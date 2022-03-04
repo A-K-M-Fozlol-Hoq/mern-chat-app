d381c4df95cb4e77250eb1a692615195423deb75
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-03-05 05:30:44 +0600

    connect mongodb with our server

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const { chats } = require('./data/data');
+const connectDB = require('./config/db');
+const colors = require('colors');
 const app = express();
 dotenv.config();
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -18,4 +21,4 @@ app.get('/api/chat/:id', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`.yellow.bold));
