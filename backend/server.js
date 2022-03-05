443ad6663d579ce2c4c6ffc19b335ffb3074e640
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-03-06 02:03:59 +0600

    creted login register ai route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const { chats } = require('./data/data');
 const connectDB = require('./config/db');
 const colors = require('colors');
+const userRoutes = require('./routes/userRoutes');
 const app = express();
 dotenv.config();
 connectDB();
@@ -10,15 +11,15 @@ connectDB();
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+app.use('/api/user', userRoutes);
+// app.get('/api/chat', (req, res) => {
+//   res.send(chats);
+// });
 
-app.get('/api/chat', (req, res) => {
-  res.send(chats);
-});
-
-app.get('/api/chat/:id', (req, res) => {
-  const singleChat = chats.find((chat) => chat._id === req.params.id);
-  res.send(singleChat);
-});
+// app.get('/api/chat/:id', (req, res) => {
+//   const singleChat = chats.find((chat) => chat._id === req.params.id);
+//   res.send(singleChat);
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started on PORT ${PORT}`.yellow.bold));
