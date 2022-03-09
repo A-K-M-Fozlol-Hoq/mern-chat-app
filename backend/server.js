1e8ac32897fcd815d3021b5e961963ad1d86e7b5
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-03-10 05:01:12 +0600

    created search user API

--- backend/server.js
+++ backend/server.js
@@ -13,13 +13,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 app.use('/api/user', userRoutes);
-// app.get('/', (req, res) => {
-//   res.send('chats');
+// app.get('/api/chat', (req, res) => {
+//   res.send(chats);
 // });
-app.get('/api/chat', (req, res) => {
-  res.send(chats);
-  // res.send('chats');
-});
 
 // app.get('/api/chat/:id', (req, res) => {
 //   const singleChat = chats.find((chat) => chat._id === req.params.id);
