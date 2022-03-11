1de74165509e1109d7a08000096fb940db691483
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-03-11 20:38:34 +0600

    created header section of header page

--- frontend/src/components/miscellaneous/SideDrawer.js
+++ frontend/src/components/miscellaneous/SideDrawer.js
@@ -1,7 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Box, Text } from '@chakra-ui/layout';
+import {
+  Tooltip,
+  Menu,
+  MenuButton,
+  Avatar,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+} from '@chakra-ui/react';
+import { Button } from '@chakra-ui/button';
+import { BellIcon, ChevronDownIcon } from '@chakra-ui/icons';
+import { chatState } from '../../context/chatProvider';
+import ProfileModal from './ProfileModal';
 
 const SideDrawer = () => {
-  return <div>SideDrawer</div>;
+  const [search, setSearch] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadingChat, setLoadingChat] = useState();
+  const { user } = chatState();
+  return (
+    <>
+      <Box
+        d="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        bg="white"
+        w="100%"
+        p="5px 100x 5px 10px"
+        borderWidth="5px"
+      >
+        <Tooltip label="search Users to chat" hasArrow placement="bottom-end">
+          <Button variant="ghost">
+            <i className="fas fa-search"></i>
+            <Text d={{ base: 'none', md: 'flex' }} px="4">
+              Search User
+            </Text>
+          </Button>
+        </Tooltip>
+        <Text fontSize="2xl" fontFamily="Work sans">
+          Talk-A-Tive
+        </Text>
+        <div>
+          <Menu>
+            <MenuButton p={1}>
+              <BellIcon fontSize="2xl" m={1} />
+            </MenuButton>
+            {/* <MenuList></MenuList> */}
+          </Menu>
+          <Menu>
+            <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+              <Avatar
+                size="sm"
+                cursor="pointer"
+                name={user.name}
+                src={user.pic}
+              />
+            </MenuButton>
+            <MenuList>
+              <ProfileModal user={user}>
+                <MenuItem>My Profile</MenuItem>
+              </ProfileModal>
+
+              <MenuDivider />
+              <MenuItem>Logout</MenuItem>
+            </MenuList>
+          </Menu>
+        </div>
+      </Box>
+    </>
+  );
 };
 
 export default SideDrawer;
